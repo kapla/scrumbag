e4003c1dbbfc8e751a3c2280bd07bf21a930cb46
shady <shadypierre@gmail.com>
2013-06-28 01:01:38 +0200

    - fix jms config - add user fixture

---
+++ src/Sb/AppBundle/DataFixtures/ORM/LoadFixtureData.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Sb\AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Nelmio\Alice\Fixtures;
+
+/**
+ * Load fixtures
+ */
+class LoadFixtureData extends AbstractFixture implements ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        // Load seeds and fixtures
+        $files = array();
+        $files[] = __DIR__ . '/user.yml';
+
+        Fixtures::load($files, $manager, array('providers' => array($this), 'locale' => 'fr_Fr'));
+        $manager->flush();
+    }
+}
