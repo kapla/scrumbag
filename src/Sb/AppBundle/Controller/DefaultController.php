10248dd535148546ea26ce87489e7aff3dba5cb6
Julien Sanchez <sumbobyboys@gmail.com>
2013-06-28 00:33:19 +0200

    conf sp_bower et init prod
    Signed-off-by: Julien Sanchez <sumbobyboys@gmail.com>

--- src/Sb/AppBundle/Controller/DefaultController.php
+++ src/Sb/AppBundle/Controller/DefaultController.php
@@ -3,11 +3,17 @@
 namespace Sb\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    /**
+     * @Route("/")
+     * @Template()
+     */
+    public function indexAction()
     {
-        return $this->render('SbAppBundle:Default:index.html.twig', array('name' => $name));
+        return array();
     }
 }
