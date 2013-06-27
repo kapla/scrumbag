93782324d1219036b5111b867e6bb405637a0e94
Julien Sanchez <sumbobyboys@gmail.com>
2013-06-28 01:06:23 +0200

    devreset add
    Signed-off-by: Julien Sanchez <sumbobyboys@gmail.com>

---
+++ devreset.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "$1" == "cache" -o "$1" == "" ]; then
+    rm -rf ./app/logs/*;
+    rm -rf ./app/cache/*;
+
+    php ./app/console cache:warmup --env=dev;
+    php ./app/console assetic:dump --env=dev;
+    php ./app/console assets:install --env=dev;
+
+    chmod -R 777 ./app/logs/;
+    chmod -R 777 ./app/cache/;
+fi
+
+if [ "$1" == "db" -o "$1" == "" ]; then
+    php app/console doctrine:database:drop --env=dev --force
+    php app/console doctrine:database:create --env=dev
+    php app/console doctrine:schema:create --env=dev
+
+    php app/console doctrine:fixtures:load --no-interaction
+fi
